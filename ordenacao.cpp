a155abfad5062338a2b85545464059edb6ecbfd7
caio guimaraes <caio.b.vilar@gmail.com>
2017-03-09 11:24:22 -0300

    atualizacao do trabalho de ordenacao

--- ordenacao.cpp
+++ ordenacao.cpp
@@ -1,24 +1,20 @@
 /*
- *  ordenacao.cpp
+ *  sortcompare.cpp
  *  
  *
- *  Created by Luiz Affonso Guedes on 06/03/17.
- *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *  Created by Caio José Borba Vilar Guimarães.
  *
+ *	Codigo baseado no ordenacao.cpp do Prof. Luiz Affonso - DCA UFRN.
  */
 
-// UFRN-CT-DCA
-// Programa: programa para ordenação de um vetor
-
-// Manipulação de tempo em c,c++
-// Programa: programa que usa manipuladores de tempo para 
-// medir o desempenho de algoritmos de ordenação
 
 
-#include <cstdlib>   //qsort
-#include  <time.h>   // clock(),time()
-#include <stdio.h>   // printf()
-#include <stdlib.h>  // exit()
+#include <cstdlib>   	//
+#include <ctime>    	// clock(),time()
+#include <iostream> 	//std::cout
+#include <string>			//std::string
+#include <map>				//map
+#include <vector>
 
 
 //Vetores usados pelos métodos de ordenação
@@ -27,20 +23,34 @@ int *vetorBubbleSort;
 int tamanho;
 
 //Função usada pelo qsort para comparar dois numeros
-int compare_ints( const void* a, const void* b ) {
+int compare_ints( const void* a, const void* b )
+{
 	int* arg1 = (int*) a;
 	int* arg2 = (int*) b;
-	if( *arg1 < *arg2 ) return -1;
-	else if( *arg1 == *arg2 ) return 0;
-	else return 1;
+	if( *arg1 < *arg2 )
+	{
+		return -1;
+	}
+	else if( *arg1 == *arg2 )
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
 //Algoritmos de ordenação bubble sort
-void bubbleSort(int *vetor, int tamanho) {
+void bubbleSort(int *vetor, int tamanho)
+{
 	int aux;
-	for (int i = 0; i < tamanho-1; i++) {
-		for (int j = 0; j < tamanho-1; j++) {
-			if (vetor[j] > vetor[j+1]) {
+	for (int i = 0; i < tamanho-1; i++)
+	{
+		for (int j = 0; j < tamanho-1; j++)
+		{
+			if (vetor[j] > vetor[j+1])
+			{
 				aux = vetor[j];
 				vetor[j] = vetor[j+1];
 				vetor[j+1] = aux;
@@ -53,26 +63,44 @@ void bubbleSort(int *vetor, int tamanho) {
 //Observe que os números são gerados aleatoriamente baseados
 //em uma semente. Se for passado a mesma semente, os 
 //números aleatórios serão os mesmos
-void criarVetor(int tamanhoVetor, int semente){
+void criarVetor(int tamanhoVetor, int semente)
+{
 	srand (semente);
 	vetorQuickSort = new int[tamanhoVetor];
 	vetorBubbleSort = new int[tamanhoVetor];
-	for (int i=0;i<tamanhoVetor;i++){
+	for (int i=0;i<tamanhoVetor;i++)
+	{
 		vetorQuickSort[i] =  rand()%100000;
-        vetorBubbleSort[i] = vetorQuickSort[i]; // utilizar os mesmos valores
+    vetorBubbleSort[i] = vetorQuickSort[i]; // utilizar os mesmos valores
 		//vetorBubbleSort[i] = rand()%100000;
 	}
 }
 
+void geratesteBubble(int` numvetor,int seed)
+{
+	clock_t clock_1, clock_2;
+	std::map<std::string,double> resultMap;
+	//Criar vetor com elementos aleatorios[0,100000] 
+	criarVetor(numvetor,23);
+	clock_1 = clock();
+	bubbleSort(vetorBubbleSort,numvetor);
+	clock_2 = clock();
+	//1000 10.000 100.000
+	std::cout << "Tempo de utilização de CPU em segundos: " << ((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC) << std::endl;
+	resultMap.insert(std::pair<std::string,double>("BubbleSort - 1000",((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC)));
+	delete vetorQuickSort;
+	delete vetorBubbleSort;
+}
 
 
 int main ()
 {
 	//Tamanho do vetor
 	clock_t clock_1, clock_2;
+	int n=1000;
 	std::map<std::string,double> resultMap;
 	//Criar vetor com elementos aleatorios[0,100000] 
-	criarVetor(1000,23);
+	criarVetor(n,23);
 	//Ordenar utilizando quickSort
 	clock_1 = clock();
 	qsort (vetorQuickSort, n, sizeof(int), compare_ints);
@@ -86,7 +114,48 @@ int main ()
 	//1000 10.000 100.000
 	std::cout << "Tempo de utilização de CPU em segundos: " << ((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC) << std::endl;
 	resultMap.insert(std::pair<std::string,double>("BubbleSort - 1000",((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC)));
-	exit(0);
+	delete vetorQuickSort;
+	delete vetorBubbleSort;
+	//Criar vetor com elementos aleatorios[0,100000] 
+	n = 10000; //10.000 elementos no vetor de sorting
+	criarVetor(n,23);
+	//Ordenar utilizando quickSort
+	clock_1 = clock();
+	qsort (vetorQuickSort, n, sizeof(int), compare_ints);
+	clock_2 = clock();
+	std::cout << "Tempo de utilização de CPU em segundos : " << ((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC) << std::endl;
+	resultMap.insert(std::pair<std::string,double>("QuickSort - 10000",((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC)));
+	//Ordenar utilizando bubleSort
+	clock_1 = clock();
+	bubbleSort(vetorBubbleSort,n);
+	clock_2 = clock();
+	//1000 10.000 100.000
+	std::cout << "Tempo de utilização de CPU em segundos: " << ((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC) << std::endl;
+	resultMap.insert(std::pair<std::string,double>("BubbleSort - 10000",((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC)));
+	delete vetorQuickSort;
+	delete vetorBubbleSort;
+	//Criar vetor com elementos aleatorios[0,100000] 
+	n = 100000; //10.000 elementos no vetor de sorting
+	criarVetor(n,23);
+	//Ordenar utilizando quickSort
+	clock_1 = clock();
+	qsort (vetorQuickSort, n, sizeof(int), compare_ints);
+	clock_2 = clock();
+	std::cout << "Tempo de utilização de CPU em segundos : " << ((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC) << std::endl;
+	resultMap.insert(std::pair<std::string,double>("QuickSort - 100000",((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC)));
+	//Ordenar utilizando bubleSort
+	clock_1 = clock();
+	bubbleSort(vetorBubbleSort,n);
+	clock_2 = clock();
+	//1000 10.000 100.000
+	std::cout << "Tempo de utilização de CPU em segundos: " << ((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC) << std::endl;
+	resultMap.insert(std::pair<std::string,double>("BubbleSort - 100000",((double)(clock_2-clock_1)/(double)CLOCKS_PER_SEC)));
+	delete vetorQuickSort;
+	delete vetorBubbleSort;
+	std::map<std::string,double>::iterator itr;
+	for(itr = resultMap.begin();itr != resultMap.end();itr++)
+	{
+		std::cout << (*itr).first << ": " << (*itr).second << std::endl;
+	}
+	return 0;
 }
-
-
